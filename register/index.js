06959d4a431579fd4b19d295551ba395b1b12254
evanysilvestre <evany.120@gmail.com>
2025-10-23 20:03:58 -0300

    add find by random

--- register/index.js
+++ register/index.js
@@ -1,18 +1,24 @@
 
 async function fetchData() {
     try{
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        //const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
 
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        let number = Math.floor(Math.random() * 50) + 1;
+
+        const response = await fetch(`https://pokeapi.co/api/v2/item/${number}/`);
+        console.log(response);
 
         if(!response.ok) {
             throw new Error("Could not fetch resource");
         }
 
         const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
+        const pokemonSprite = data.sprites.default;
+        const name = data.name;
+        const pokemonName = document.getElementById("pokemonName");
         const imgElement = document.getElementById("pokemonSprite");
 
+        pokemonName.innerHTML = name;
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
 
